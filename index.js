a8a86b5227ab802ede79ab1efcfa98861a01ac9b
Alexander <al.x.falconer@gmail.com>
2021-04-08 19:06:19 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+function countDown(num) {
+    while (num >= 0) {
+        console.log(num)
+        num -=1;
+    }
+}
+
+function writeCards (names, birthday) {
+    let cards = [];
+    for (let i = 0; i < names.length; i++) {
+        cards.push(`Thank you, ${names[i]}, for the wonderful ${birthday} gift!`);
+    }
+    return cards;
+}
